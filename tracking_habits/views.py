17096218a199282b57c630ddae902eb17904439e
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-06-11 14:41:05 +0500

    add views for model Habit in app tracking habits

--- tracking_habits/views.py
+++ tracking_habits/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from tracking_habits.models import Habit
+from tracking_habits.paginators import TrackingHabitsPaginator
+
 
 # Create your views here.
+class HabitViewSet(viewsets.ModelViewSet):
+    queryset = Habit.objects.all()
+    pagination_class = TrackingHabitsPaginator
