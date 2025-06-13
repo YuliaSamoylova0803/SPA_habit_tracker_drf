a7b3e53636bd50a3262c4404d82ee6a815471a4f
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-06-14 00:46:18 +0500

    add permissions in app users

---
+++ users/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsOwner(BasePermission):
+    """
+    Разрешает доступ только владельцу объекта.
+    """
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
