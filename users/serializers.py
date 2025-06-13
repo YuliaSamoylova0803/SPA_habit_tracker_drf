fe9485f31dc423f965a5a9badca1fbff4f0a2141
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-06-13 23:54:12 +0500

    update serializers in app users

---
+++ users/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from users.models import User
+
+
+class UserSerializer(ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = "__all__"
+
+
+class OtherUserProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "city", "avatar", "tg_nick"]
+        read_only_fields = fields
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "email",
+            "phone_number",
+            "city",
+            "avatar",
+            "tg_nick",
+        ]
+
+        read_only_fields = ["id", "email"]
