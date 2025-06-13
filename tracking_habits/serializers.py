4d76abde2c0e818130ae9b7c9e524b98a6c8b5b4
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-06-14 00:48:56 +0500

    update serializers in app tracking habits

--- tracking_habits/serializers.py
+++ tracking_habits/serializers.py
@@ -7,3 +7,11 @@ class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
         fields = "__all__"
+        read_only_fields = ("user", "created_at", "last_completed")
+
+
+class PublicHabitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Habit
+        fields = ["id", "place", "time", "action", "duration", "is_public"]
+        read_only_fields = fields
