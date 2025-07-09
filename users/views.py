42afa785e42e135d28f9ff9ede63362d29366fae
Юлия Самойлова <Ulia629736@yandex.ru>
2025-07-09 23:30:08 +0500

    update after isort files

--- users/views.py
+++ users/views.py
@@ -4,11 +4,8 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 
 from .models import User
-from .serializers import (
-    OtherUserProfileSerializer,
-    UserProfileSerializer,
-    UserSerializer,
-)
+from .serializers import (OtherUserProfileSerializer, UserProfileSerializer,
+                          UserSerializer)
 
 
 # Create your views here.
